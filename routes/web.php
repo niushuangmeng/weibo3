e002d7296cbe7d1be10c6ca39c9754be5833dce4
vagrant <vagrant@homestead>
2019-05-11 06:29:46 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/','StaticPagesController@home');//第一个参数指明了 URL，第二个参数指明了处理该 URL 的控制器动作。
+Route::get('/help','StaticPagesController@help');
+Route::get('/about','StaticPagesController@about');
